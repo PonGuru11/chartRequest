deb4223bdcc84480cab79d1951c48a8ec8aa32bb
Zylen-Guru <gurunathan.s@zylensolutions.com>
2024-08-13 18:35:36 +0530

    test

--- Controler/PatientChart/ChartPrint/ChartPrint.js
+++ Controler/PatientChart/ChartPrint/ChartPrint.js
@@ -84,7 +84,7 @@ await sleep(5000);
         await page.keyboard.press('KeyP', { delay: 5000 });
         await page.keyboard.up('Control');
         patientChartLogger.info("PrintOption Selected Successfully...");
-        await sleep(10000)
+        await sleep(20000)
     }catch (error) {
        console.log("Error:",error)
        return res.status(500).json({ message: "Error in Record View Page function" });
